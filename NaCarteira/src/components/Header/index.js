1ee8eed2ba077851875c90f5224c7508ac8a1dc7
Ryan <96851297+oRyanMS@users.noreply.github.com>
2023-09-01 09:37:42 -0300

    criação do app

--- NaCarteira/src/components/Header/index.js
+++ NaCarteira/src/components/Header/index.js
@@ -3,19 +3,55 @@ import{
     View,
     StyleSheet,
     Text,
-    StatusBar
+    StatusBar,
+    TouchableOpacity,
 } from 'react-native'
 
-export default function Header(){
+import { Feather } from '@expo/vector-icons'
+
+
+const statusBarHeigth = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
+
+export default function Header({name}){
     return(
         <View style={styles.container}>
-            <Text> Header do App</Text>
+            <View style={styles.content}>
+                <Text style={styles.username}>{name}</Text>
+
+                <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+                    <Feather name='user' size={27} color={"#fff"}/>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: '#aaff'
+        backgroundColor: '#8000ff',
+        paddingTop: statusBarHeigth,
+        flexDirection: 'row',
+        paddingStart: 16,
+        paddingEnd: 16,
+        paddingBottom: 44,
+    },
+    content:{
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    username:{
+        fontSize: 18,
+        color: '#fff',
+        fontWeight: 'bold'
+    },
+    buttonUser:{
+        width: 44,
+        height: 44,
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 44/2,
     }
 })
--- NaCarteira/src/pages/Home/index.js
+++ NaCarteira/src/pages/Home/index.js
@@ -1,12 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta luz',
+    value: '300,80',
+    date: '17/09/2023',
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: 'Pix',
+    value: '1.200,00',
+    date: '14/09/2023',
+    type: 1 //entrada
+  },
+  {
+    id: 3,
+    label: 'Pix Mãe',
+    value: '100,00',
+    date: '21/09/2023',
+    type: 0 //despesas
+  },
+]
+
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        <Header/>
-      <Text>Ryan Maximiano</Text>
+        <Header name="Ryan Maximiano"/>
+
+        <Balance saldo="9.250,90" gastos="-527,00"/>
+
+        <Text style={styles.title}> Últimas movimentações</Text>
+
     </View>
   );
 }
@@ -14,6 +44,13 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dadada',
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
   },
 });
